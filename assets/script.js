73bbd146dd7002031bf96e3eb526c9e21d7b49f3
[Bryan Dalton] <[bryantdalton19@gmail.com]>
2022-09-25 21:47:22 -0400

    more api testing and minimal button functionality

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,34 @@
 //to be added
 var chicagoArtApi = "https://api.artic.edu/api/v1/artworks";
+var infoAside = document.querySelector('.info')
+var homeBtn = document.getElementById('home')
+var colorBtns = document.querySelectorAll('.color-btn')
+var colorDiv = document.getElementById('color-div')
 
 
+console.log(colorBtns)
+// calls data of artwork from the api but all it really does right now is append their titles to the display page
 fetch(chicagoArtApi)
 .then (response => response.json())
-.then(data => console.log(data));
+.then(({ data }) => appendTitle(data));
+//append title function works but throws error on index.html because it's not appending to that page
+const appendTitle = (artData) => {
+    artData.forEach((art) => {
+    var titleEl = document.createElement('h3')
+    titleEl.innerText = art.title
+    infoAside.appendChild(titleEl);
+    });
+};
+
+
+function returnHome () {
+    document.location = './index.html'
+};
+
+function btnHandler() {
+    document.location = './display.html'
+};
+
+
+homeBtn.addEventListener('click', returnHome);//works but throws error on index.html
+colorDiv.addEventListener('submit', console.log(click));//doesn't work
