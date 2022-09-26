a9acf6826c5c3fbfa7ec38ebd80c689dd04b8697
[Bryan Dalton] <[bryantdalton19@gmail.com]>
2022-09-26 18:53:18 -0400

    appended art titles to display page and buttons have functionality

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,18 @@
-//to be added
 var chicagoArtApi = "https://api.artic.edu/api/v1/artworks";
 var infoAside = document.querySelector('.info')
-var homeBtn = document.getElementById('home')
+var homeBtn = document.querySelector('#home')
 var colorBtns = document.querySelectorAll('.color-btn')
-var colorDiv = document.getElementById('color-div')
+var colorDiv = document.querySelector('#color-div')
+var pathName = window.location.pathname;
+var pageArray = pathName.split("/");
+var page = pageArray[pageArray.length - 1]
+console.log(page);
 
-
-console.log(colorBtns)
 // calls data of artwork from the api but all it really does right now is append their titles to the display page
-fetch(chicagoArtApi)
-.then (response => response.json())
-.then(({ data }) => appendTitle(data));
+
+
 //append title function works but throws error on index.html because it's not appending to that page
-const appendTitle = (artData) => {
+function appendTitle (artData) {
     artData.forEach((art) => {
     var titleEl = document.createElement('h3')
     titleEl.innerText = art.title
@@ -22,13 +22,25 @@ const appendTitle = (artData) => {
 
 
 function returnHome () {
+    page = 'index.html';
     document.location = './index.html'
+    console.log('this is return home function')
 };
 
-function btnHandler() {
+function btnHandler() { 
+    page = 'diplay.html';
     document.location = './display.html'
+    console.log('this is btnHandler fucnction')
 };
 
+if (page == 'index.html') {
+colorDiv.addEventListener('click', btnHandler);
 
-homeBtn.addEventListener('click', returnHome);//works but throws error on index.html
-colorDiv.addEventListener('submit', console.log(click));//doesn't work
+} else {
+    fetch(chicagoArtApi)
+.then (response => response.json())
+.then(({ data }) => appendTitle(data));
+    homeBtn.addEventListener('click', returnHome);
+
+}
+
